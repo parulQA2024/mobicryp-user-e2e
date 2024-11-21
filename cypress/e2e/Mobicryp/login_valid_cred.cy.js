69041a13bf1980ebf29b2698fd82f9cb88e462a7
parulQA2024 <parul.singh@atrox.io>
2024-11-21 15:22:55 +0530

    new

--- cypress/e2e/Mobicryp/login_valid_cred.cy.js
+++ cypress/e2e/Mobicryp/login_valid_cred.cy.js
@@ -1,10 +1,7 @@
 describe('Login and Redirection Test', () => {
   
     it('should log in and redirect to the dashboard', () => {
-      
-      cy.visit('https://mobicryp-demo-uat.crypquesupport.com/login'); 
-      cy.get(`input[name="email"]`).type('MC400001');
-      cy.get(`input[name="password"]`).type('Password@123'); 
+    
   
       
       cy.get('button').contains('Login').click();
