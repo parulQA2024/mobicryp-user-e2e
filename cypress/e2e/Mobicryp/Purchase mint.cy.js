5d47de6e1cc4df311f6ee67ea1b69c019f2b35ab
parulQA2024 <parul.singh@atrox.io>
2025-01-30 16:00:47 +0530

    new changes

--- cypress/e2e/Mobicryp/Purchase mint.cy.js
+++ cypress/e2e/Mobicryp/Purchase mint.cy.js
@@ -19,51 +19,54 @@ describe(
   
         cy.get(`input[name="pin"]`).type("1234");
   
-        //cy.contains('button', 'Submit').click();
-        // cy.get(`input[class="flex items-center"]`).contains('Hub').click();
-        // cy.get(`input[class="flex items-center gap-2"]`).contains('Buy Hub').click();
-        cy.contains("Hub").click();
-        cy.contains("Buy Hub").click();
+        // cy.contains('button', 'Submit').click();
+      //   // cy.get(`input[class="flex items-center"]`).contains('Hub').click();
+      //   // cy.get(`input[class="flex items-center gap-2"]`).contains('Buy Hub').click();
+      //   cy.contains("Hub").click();
+      //   cy.contains("Buy Hub").click();
   
-        cy.contains("button", "Select Package").click(); // select package
-        cy.contains("Request OTP").click();
-        cy.get(`input[name="otp"]`).type("4152");
-        cy.wait(300)
-        cy.contains('button','Confirm').click();
-      //   cy.get("input[name]").click();
-        cy.wait(300);
-        cy.reload();
+      //   cy.contains("button", "Select Package").click(); // select package
+      //   cy.contains("Request OTP").click();
+      //   cy.get(`input[name="otp"]`).type("4152");
+      //   cy.wait(300)
+      //   cy.contains('button','Confirm').click();
+      // //   cy.get("input[name]").click();
+      //   cy.wait(300);
+      //   cy.reload()
 
-        cy.contains("Mint").last().click(); // Expand the Minting menu
-        cy.contains('Mint Purchase').click(); // Click on Mint Purchase
-    
+        cy.visit("https://mobicryp-demo-uat.crypquesupport.com/mint-purchase"); //visit url
+
+        //cy.contains('Mint Purchase').click(); // Click on Mint Purchase
+        
         // Verify the URL
-        cy.visit("https://mobicryp-demo-uat.crypquesupport.com/mint-purchase");
+        //cy.visit("https://mobicryp-demo-uat.crypquesupport.com/mint-purchase");
 
         cy.url().should('include', '/mint-purchase');
         cy.contains('Mint Purchase').should('be.visible');
     
-        // Select "USDT Wallet Balance"
+        // check "USDT Wallet Balance" and fund wallet 
         cy.contains('USDT Wallet Balance').should('be.visible');
         cy.contains('Fund Wallet Balance').should('be.visible');
+        cy.contains('Others').should('be.visible');
 
     
         // Select "Myself" for the "Topup Wallet for" option
         cy.contains('Myself').click();
-    
+        cy.contains('Select Hub for Capacity').click();
+        cy.wait(3000);
         // Select a Hub Package from the dropdown
-        cy.get('--Select Package--').click().select('Basic | Total Capacity : $ 300 | Remaining Capacity : $ 150'); // Adjust selector and text as needed
-    
+        //cy.contains('Basic|Total Capacity:$ 300|Remaining Capacity:$ 150').click();
+        cy.get("select").select("Basic | Total Capacity : $ 300 | Remaining Capacity : $ 150 ").invoke("value").should("eq", "1");
         // Enter capacity in the input field
-        cy.get('input[placeholder="Enter the HUB Capacity"]').type('150'); // Replace placeholder with the actual value if different
+        // cy.get('input[placeholder="Enter the HUB Capacity"]').type('150'); // Replace placeholder with the actual value if different
     
-        // Select "Self" for the mode
-        cy.contains('Self').click();
+        // // Select "Self" for the mode
+        // cy.contains('Self').click();
 
-        cy.contains("Enter OTP").click();
-        cy.get(`input[name="otp"]`).type("4152");
-        cy.wait(300)
-        cy.contains('button','Purchase').click();
+        // cy.contains("Enter OTP").click();
+        // cy.get(`input[name="otp"]`).type("4152");
+        // cy.wait(300)
+        // cy.contains('button','Purchase').click();
 
       })
 });
